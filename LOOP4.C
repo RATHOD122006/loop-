bc3640a549b7f63d001a689dd2c86ca26e72731c
RATHOD122006 <sumitrathod8225@gmail.com>
2024-02-05 20:57:17 +0530

    loop

---
+++ LOOP4.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+int a=15;
+clrscr();
+while(a>=1){
+printf("%d",a);
+a=a-2;
+}
+getch();
+}
---
+++ LOOP5.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+int a=2020;
+clrscr();
+while(a<=2040){
+if(a%4);
+printf("%d",a);
+a=a+4;
+}
+getch();
+}
